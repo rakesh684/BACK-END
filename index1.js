ed8618d95e1e945e1b2f6f52f4d6faca74193c9f
Rakesh kumar <dr.rakesh684@gmail.com>
2021-11-07 18:12:38 +0530

    serving html from a file

--- index1.js
+++ index1.js
@@ -1,6 +1,10 @@
 // creating our first server
 const http = require('http');
 const port = 8000;
+
+// file module fs is reqire
+const fs = require('fs');
+
 //creating request handler
 function requestHandler(req,res){
   console.log(req.url);
@@ -8,10 +12,19 @@ function requestHandler(req,res){
   // serving HTML
 
   res.writeHead(200,{'content-type':'text/html'});
-  res.end('<h1> We Got It</h1>');
+  // res.end('<h1> We Got It</h1>');
+
+  // reading html file here
+  fs.readFile('./index.html', function(err,data){
+    if(err){
+      console.log('error',err);
+      return res.end('<h1>Error !!</h1>');
+    }
+    return res.end(data);
+  })
 }
 
-// serving HTML
+// Reading and serving HTML from file
 
 
 
