d2c5d3de999622dabaff5601913e3c4599c2b679
Rakesh kumar <dr.rakesh684@gmail.com>
2021-11-07 17:44:25 +0530

    Adding request handler

--- index1.js
+++ index1.js
@@ -1,11 +1,13 @@
 // creating our first server
 const http = require('http');
 const port = 8000;
+//creating request handler
+function requestHandler(req,res){
+  console.log(req.url);
+  res.end('We Got It');
+}
 
-
-
-
-const server = http.createServer();
+const server = http.createServer(requestHandler);
 server.listen(port,function(err){
   if(err){
     console.log(err);
