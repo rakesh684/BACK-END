e049c642f2c0f9cc5b0709e945b6709de1a44a77
Rakesh kumar <dr.rakesh684@gmail.com>
2021-11-07 22:07:23 +0530

    executing multiple htmla pages

--- index1.js
+++ index1.js
@@ -1,38 +1,46 @@
 // creating our first server
 const http = require('http');
-const port = 8000;
+const port =8080;
 
 // file module fs is reqire
-const fs = require('fs');
+const fs=require('fs');
 
-//creating request handler
-function requestHandler(req,res){
+function requesthandler(req , res){
   console.log(req.url);
   // res.end('We Got It');
   // serving HTML
-
   res.writeHead(200,{'content-type':'text/html'});
   // res.end('<h1> We Got It</h1>');
 
-  // reading html file here
-  fs.readFile('./index.html', function(err,data){
+  //executing multiple html pages
+  let filePath;
+  switch(req.url){
+    case '/':
+      filePath='./index.html'
+      break;
+    
+    case '/profile':
+      filePath='./profile.html'
+      break;
+    
+    default:
+      filePath ='./404.html'
+  }
+  fs.readFile(filePath, function(err,data){
     if(err){
       console.log('error',err);
-      return res.end('<h1>Error !!</h1>');
+      return res.end('<h1> Eror! </h1>');
     }
     return res.end(data);
-  })
+  });
+  
 }
-
-// Reading and serving HTML from file
-
-
-
-const server = http.createServer(requestHandler);
-server.listen(port,function(err){
+const server = http.createServer(requesthandler);
+server.listen(port ,function(err)
+{
   if(err){
     console.log(err);
     return;
   }
-  console.log("Server is up and running on port",port);
-});
+  console.log("server is up and running on port",port);
+})
