4a7541ed54c46cae5d49270d97acef083fd286a4
Rakesh kumar <dr.rakesh684@gmail.com>
2021-11-07 17:38:38 +0530

    Creating first server

---
+++ index1.js
@@ -0,0 +1,15 @@
+// creating our first server
+const http = require('http');
+const port = 8000;
+
+
+
+
+const server = http.createServer();
+server.listen(port,function(err){
+  if(err){
+    console.log(err);
+    return;
+  }
+  console.log("Server is up and running on port",port);
+});
