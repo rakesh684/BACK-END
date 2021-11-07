cd9713d66286195504199ed9a19185d9726fdca6
Rakesh kumar <dr.rakesh684@gmail.com>
2021-11-07 17:51:15 +0530

    Adding request handler

--- index1.js
+++ index1.js
@@ -4,8 +4,16 @@ const port = 8000;
 //creating request handler
 function requestHandler(req,res){
   console.log(req.url);
-  res.end('We Got It');
+  // res.end('We Got It');
+  // serving HTML
+
+  res.writeHead(200,{'content-type':'text/html'});
+  res.end('<h1> We Got It</h1>');
 }
+
+// serving HTML
+
+
 
 const server = http.createServer(requestHandler);
 server.listen(port,function(err){
